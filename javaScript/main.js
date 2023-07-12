b9c06c0efe83ee4e615f52cdd27b329ca5132398
TatuzinhoBixo <celuw_lima@hotmail.com>
2023-07-12 11:04:50 -0400

    almost, but OK

--- javaScript/main.js
+++ javaScript/main.js
@@ -12,13 +12,13 @@ form.addEventListener('submit', function(e) {
 
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
 
     formEValido = validaNome(nomeBeneficiario.value)    
     if (formEValido) {
         const containerMansagemSucesso = document.querySelector('.success-message');
         containerMansagemSucesso.innerHTML = mensagemSucesso;
-        containerMansagemSucesso.computedStyleMap.display = 'block';
+        containerMansagemSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         nomeContaBeneficiario.value = '';
@@ -34,10 +34,10 @@ nomeBeneficiario.addEventListener('keyup', function(e) {
     formEValido = validaNome(e.target.value);
 
     if (!formEValido) {
-        nomeContaBeneficiario.classList.add('error');
+        nomeBeneficiario.classList.add('error');
         document.querySelector('.error-message').style.display = 'block'; 
     } else {
-        nomeBeneficiario.remove('error');
+        nomeBeneficiario.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
     }
 });
