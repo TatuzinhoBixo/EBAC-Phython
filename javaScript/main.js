ba703350c15e53b82fffd7bf5d3cfd32cba4bba0
Tatu Rededondo <email@example.com>
2023-07-11 21:52:50 -0400

    javascript 2

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,26 +8,36 @@ function validaNome(nomeCompleto) {
 }
 
 form.addEventListener('submit', function(e) {
-    let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
 
     formEValido = validaNome(nomeBeneficiario.value)    
     if (formEValido) {
-        alert(mensagemSucesso);
+        const containerMansagemSucesso = document.querySelector('.success-message');
+        containerMansagemSucesso.innerHTML = mensagemSucesso;
+        containerMansagemSucesso.computedStyleMap.display = 'block';
 
         nomeBeneficiario.value = '';
-        nomeContaBeneficiario.valu = '';
+        nomeContaBeneficiario.value = '';
         valorDeposito.value = '';
-    }
-    else {
-        alert("O nome não está completo");
-    
+    } else {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-console.log(form)
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value);
+
+    if (!formEValido) {
+        nomeContaBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block'; 
+    } else {
+        nomeBeneficiario.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
