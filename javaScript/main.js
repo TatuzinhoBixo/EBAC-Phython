83dc33c76915176989eb0ef8ed3bf7d651d9aa90
TatuzinhoBixo <celuw_lima@hotmail.com>
2023-07-26 16:51:46 -0400

    ebac-novo-exercicio7

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,43 +1,27 @@
 const form = document.getElementById('form-deposito');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
-let formEValido = false;
 
-function validaNome(nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
+function validaNome(nomeCompleto) {    /*Função que valida o nome, se tem sobrenome */
+    const nomeComoArray = nomeCompleto.split(' ');    /*transforma o nome com o array separado por espaço */
     return nomeComoArray.length >= 2;
 }
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
 
+form.addEventListener('submit', function(e) {    /* adiciona um evento toda x q tem um submit */
+    let formEvalido = false  /*inicia como falta para ser testado abaixo */
+    e.preventDefault();    /* cancela a ação padrão do submit */
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario'); /* pega o nome do beneficiario */
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;   /* tem que ter o .value para exibir o valor */
 
-    formEValido = validaNome(nomeBeneficiario.value)    
-    if (formEValido) {
-        const containerMansagemSucesso = document.querySelector('.success-message');
-        containerMansagemSucesso.innerHTML = mensagemSucesso;
-        containerMansagemSucesso.style.display = 'block';
-
-        nomeBeneficiario.value = '';
-        nomeContaBeneficiario.value = '';
-        valorDeposito.value = '';
+    if (validaNome(nomeBeneficiario.value)) {   /* valida o nome de acordo com a nome recebido */
+        alert(mensagemSucesso)
     } else {
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
+        alert('O nome não está completo')
     }
+     
+
 })
 
-nomeBeneficiario.addEventListener('keyup', function(e) {
-    console.log(e.target.value);
-    formEValido = validaNome(e.target.value);
-
-    if (!formEValido) {
-        nomeBeneficiario.classList.add('error');
-        document.querySelector('.error-message').style.display = 'block'; 
-    } else {
-        nomeBeneficiario.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'none';
-    }
-});
+console.log(form);
