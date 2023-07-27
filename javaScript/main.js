4ec3bb5805d80c4aef8a325e61980c872d835b86
TatuzinhoBixo <celuw_lima@hotmail.com>
2023-07-27 16:43:29 -0400

    final-antes-tarefa-cap-7

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,27 +1,43 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEvalido = false; 
 
 function validaNome(nomeCompleto) {    /*Função que valida o nome, se tem sobrenome */
     const nomeComoArray = nomeCompleto.split(' ');    /*transforma o nome com o array separado por espaço */
     return nomeComoArray.length >= 2;
 }
 
-
 form.addEventListener('submit', function(e) {    /* adiciona um evento toda x q tem um submit */
-    let formEvalido = false  /*inicia como falta para ser testado abaixo */
     e.preventDefault();    /* cancela a ação padrão do submit */
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario'); /* pega o nome do beneficiario */
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;   /* tem que ter o .value para exibir o valor */
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;   /* tem que ter o .value para exibir o valor */
 
-    if (validaNome(nomeBeneficiario.value)) {   /* valida o nome de acordo com a nome recebido */
-        alert(mensagemSucesso)
+    formEvalido = validaNome(nomeBeneficiario.value)
+    if (formEvalido) { /* valida o nome de acordo com a nome recebido */
+        const containerMesagemSucesso = document.querySelector('.success-message');   /*recebe a configuração do css */
+        containerMesagemSucesso.innerHTML = mensagemSucesso; //Recebe a menssagem de sucesso a ser exibida
+        containerMesagemSucesso.style.display = 'block';  /*Altera o parâmetro do css */
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
     } else {
-        alert('O nome não está completo')
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     }
-     
-
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEvalido = validaNome(e.target.value);
+
+    if (!formEvalido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
